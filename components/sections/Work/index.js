2635000a2db6ce5f4d6e5e9391059a8bbb0d2eef
Md Wahiduzzaman Suva <wshuvo360@gmail.com>
2025-01-26 19:36:06 +0600

    updated

--- components/sections/Work/index.js
+++ components/sections/Work/index.js
@@ -1,82 +1,180 @@
 const projects = [
   {
-    name: "Zobo",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    name: "Courseway",
+    description: "A platform for managing and tracking course materials. Built with a React.js front-end and a Laravel back-end.",
+    tags: [
+      {
+        name: "React.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Laravel",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "MySQL",
+        color: "green-text-gradient",
+      },
+    ],
+    image: "/images/courseway.png",
+    source_code_link: "https://github.com/wshuv-o/Courseway-a-Lerning-Platform-Instructors",
+  },
+  {
+    name: "Koala Tracklog Analysis",
+    description: "A data analysis project for studying koala movement patterns using Python libraries like Pandas, NumPy, and Matplotlib.",
+    tags: [
+      {
+        name: "Python",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Data Visualization",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "Scikit-learn",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "/images/koala.png",
+    source_code_link: "https://github.com/wshuv-o/Koala-Tracklog-Analysis",
+  },
+  {
+    name: "Sentiment Analysis on Conti Ransomware Leaks Data",
+    description: "Analyzed sentiment of leaked ransomware data using Vader (NLTK) and conducted network analysis with NetworkX.",
+    tags: [
+      {
+        name: "NLTK",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "NetworkX",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "Python",
+        color: "green-text-gradient",
+      },
+    ],
+    image: "/images/sentiment.png",
+    source_code_link: "https://github.com/wshuv-o/Sentiment-Analysis",
+  },
+  {
+    name: "Nexa: Social Media Desktop App",
+    description: "A desktop application supporting real-time messaging, multimedia sharing, and user profiles, built with C# and MSSQL.",
     tags: [
       {
         name: ".NET Framework",
         color: "blue-text-gradient",
       },
       {
-        name: "MySql",
-        color: "orange-text-gradient",
-      },
-      {
-        name: "webRTC",
-        color: "pink-text-gradient",
-      },
-    ],
-    
-    image: "/images/web1.png",
-    source_code_link: "https://github.com/wshuv-o/Nexa-Social-Media",
-  },
-  {
-    name: "Creative thinking",  
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-    tags: [
-      {
-        name: "Java Swing",
-        color: "blue-text-gradient",
-      },
-    ],
-    image: "/images/web2.png",
-    source_code_link: "https://github.com/wshuv-o/Railway-Ticketing-System",
-  },
-  {
-    name: "Creative money",  
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-    tags: [
-      {
-        name: "Java Swing",
-        color: "blue-text-gradient",
-      },
-    ],
-    image: "/images/web2.png",
-    source_code_link: "https://github.com/wshuv-o/Railway-Ticketing-System",
-  },
-  {
-    name: "asdfasdfasdf",  
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-    tags: [
-      {
-        name: "Java Swing",
-        color: "blue-text-gradient",
-      },
-    ],
-    image: "/images/web2.png",
-    source_code_link: "https://github.com/wshuv-o/Railway-Ticketing-System",
-  },
-  {
-    name: "Project Name",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    tags: [
-      {
-        name: "html",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "JavaScript",
+        name: "MSSQL",
         color: "green-text-gradient",
       },
       {
-        name: "css",
+        name: "WebRTC",
         color: "pink-text-gradient",
       },
     ],
-    image: "/images/web3.png",
-    source_code_link: "https://github.com/",
+    image: "/images/nexa.png",
+    source_code_link: "https://github.com/wshuv-o/Nexa-Social-Media",
   },
-  
+  {
+    name: "Ride Sharing Application",
+    description: "Developed an Android app enabling real-time ride sharing, tracking, and mapping using Java and Android SDK.",
+    tags: [
+      {
+        name: "Java",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Android SDK",
+        color: "green-text-gradient",
+      },
+    ],
+    image: "/images/ride-sharing.png",
+    source_code_link: "https://github.com/wshuv-o/Ride-Sharing-App",
+  },
+  {
+    name: "Railway Ticketing System",
+    description: "A Java Swing application with features like ticket purchasing, train tracking, and admin management.",
+    tags: [
+      {
+        name: "Java Swing",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "MySQL",
+        color: "green-text-gradient",
+      },
+    ],
+    image: "/images/railway.png",
+    source_code_link: "https://github.com/wshuv-o/Railway-Ticketing-System",
+  },
+  {
+    name: "Optra",
+    description: "Optimized task management system for small to medium-sized teams, enhancing efficiency and collaboration.",
+    tags: [
+      {
+        name: "C#",
+        color: "blue-text-gradient",
+      },
+      {
+        name: ".NET Framework",
+        color: "green-text-gradient",
+      },
+    ],
+    image: "/images/optra.png",
+    source_code_link: "https://github.com/wshuv-o/Optra",
+  },
+  {
+    name: "iInventory",
+    description: "An inventory management system developed with C#, providing real-time stock updates and reporting.",
+    tags: [
+      {
+        name: "C#",
+        color: "blue-text-gradient",
+      },
+      {
+        name: ".NET Framework",
+        color: "green-text-gradient",
+      },
+    ],
+    image: "/images/iinventory.png",
+    source_code_link: "https://github.com/wshuv-o/iInventory",
+  },
+  {
+    name: "Java Swing Projects",
+    description: "A collection of projects built with Java Swing, including management systems and utility tools.",
+    tags: [
+      {
+        name: "Java Swing",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "MySQL",
+        color: "green-text-gradient",
+      },
+    ],
+    image: "/images/swing-projects.png",
+    source_code_link: "https://github.com/wshuv-o/Java-Swing-Projects",
+  },
+  {
+    name: "C# Projects",
+    description: "Various projects leveraging C# and .NET Framework, showcasing desktop app development skills.",
+    tags: [
+      {
+        name: "C#",
+        color: "blue-text-gradient",
+      },
+      {
+        name: ".NET Framework",
+        color: "green-text-gradient",
+      },
+    ],
+    image: "/images/csharp-projects.png",
+    source_code_link: "https://github.com/wshuv-o/CSharp-Projects",
+  },
 ];
 
 export default projects;
